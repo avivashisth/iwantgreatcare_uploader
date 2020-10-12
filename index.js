b86a9c8e5e269b9fc2ae3de1dcec86a000a2ee19
avinash <avinash@extendtech.net>
2020-10-12 08:47:53 +0530

    initial commit with working model

---
+++ index.js
@@ -0,0 +1,85 @@
+const chunk = require('lodash.chunk');
+const fetch = require('node-fetch');
+const { join: pathJoin } = require('path');
+const fsExtra = require('fs-extra');
+
+const { promises: fs } = require('fs');
+
+require('dotenv').config();
+
+const {
+  API_URL,
+  AUTH_TOKEN,
+} = process.env;
+
+const getFilesList = async () => (await fs.readdir('./source'))
+  .filter(fileName => fileName.indexOf('.json') !== -1);
+
+const uploadFile = async fileName => {
+  const fileData = (await fs.readFile(pathJoin(`./source`, fileName))).toString();
+
+  const response = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      authtoken: AUTH_TOKEN,
+      'Content-Type': "application/json",
+    },
+    body: fileData,
+  });
+
+  // update stats
+  const resp = await response.json();
+
+  return {
+    fileName,
+    ok: response.ok,
+    response: resp,
+  };
+};
+
+const updateStats = async ({ data, targetFileName }) => {
+  let fileData;
+
+  try {
+    fileData = await fsExtra.readJSON(targetFileName);
+  } catch (e) {
+    fileData = [];
+  }
+
+  fileData = fileData.concat(data);
+
+  return fsExtra.writeJSON(targetFileName, fileData);
+};
+
+const uploadFilesList = ({ fileList }) => Promise.all(fileList.map(fileLocation => uploadFile(fileLocation)));
+
+const init = (async () => {
+  const fileList = await getFilesList();
+
+  for (const fileChunk of chunk(fileList, 4)) {
+    let response;
+    try {
+      response = await uploadFilesList({ fileList: fileChunk });
+
+      const errorredFileName = response.filter(stat => !stat.ok)
+        .map(stat => stat.fileName);
+
+      await updateStats({
+        data: errorredFileName,
+        targetFileName: './stats/error.json'
+      });
+    } catch (e) {
+      console.error(e);
+
+      await updateStats({
+        data: {
+          response,
+          error: e
+        },
+        targetFileName: './stats/report-error.json'
+      });
+
+      // process.exit(1);
+    }
+  }
+})();
---
+++ readme.md
@@ -0,0 +1,19 @@
+## Setting Up
+
+1. Download and install [NodeJS](https://nodejs.org/en/download/).
+2. Open console/cmd/terminal and navigate to current directory.
+3. For the first time only, run the following command
+    ```javascript
+    npm ci
+    ```
+4. Make sure to copy the JSON files in **source** directory.
+5. Rename **.env_sample** to **.env** and update parameters value defined in it, where *AUTH_TOKEN* should contain the authentication token, and *API_URL* should have the API url where data will be sent.
+6. Run the following command to start file sync
+    ```javascript
+    npm start
+    ```
+7. Successfully uploaded files will be moved to **processed** directory.
+
+#### Notes
+1. Errored files name will be created available in **./stats/error.json**.
+2. Monitor and report data in file **./stats/report-error.json** if any to admin.
